ba12cf14754d75643b77de605b7ff2e83f3875d2
Denis <51379629+D3nya@users.noreply.github.com>
2024-12-28 03:01:28 +0300

    add tailwind

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import MyButton from '../UI/button/MyButton';
-import MyInput from '../UI/input/MyInput';
+import React from "react";
+import MyButton from "../UI/button/MyButton";
+import MyInput from "../UI/input/MyInput";
 import {
   list_item,
   btn_close,
   text_checked,
   input,
   task__text,
-} from './TaskItem.module.css';
+} from "./TaskItem.module.css";
 
 const TaskItem = ({ id, text, completed, setTasks }) => {
   const toggleTask = (id, completed) => {
@@ -38,6 +38,7 @@ const TaskItem = ({ id, text, completed, setTasks }) => {
       <label className={`${task__text} ${text_checked}`} htmlFor={id}>
         {text}
       </label>
+
       <MyButton className={btn_close} onClick={() => deleteTask(id)}>
         Delete
       </MyButton>
